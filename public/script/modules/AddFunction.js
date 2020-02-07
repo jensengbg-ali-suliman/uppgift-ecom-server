5c4978ae6bae0bad700de2c0ba171626267eb2d8
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-02-07 13:50:30 +0100

    Update AddFunction.js

--- public/script/modules/AddFunction.js
+++ public/script/modules/AddFunction.js
@@ -5,12 +5,15 @@ function addFunction(arr) {
 }
 
 function addToCart(e) {
+    let elem = e.target.parentNode.parentNode;
     let target = e.target.parentNode.parentNode.id;
     let url = `http://localhost:8000/cart/add?id=${target}`;
     fetch(url, { method: 'POST' })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => console.log(data.message))
         .catch(err => console.error(err));
+
+    elem.classList.add('checked');
 }
 
 
